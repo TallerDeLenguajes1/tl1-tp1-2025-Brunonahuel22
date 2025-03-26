29a0d5a20948e20c5bea03819d81873e5d617b7e
Bruno Cordoba <brunonahuel87@gmail.com>
2025-03-26 08:39:31 -0300

    agregue la tarea solicitada de sobre punteros

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hola mundo");
-    
+int main()
+{
+    printf("Hola mundo\n");
+    int numero = 5, *pnumero = &numero;
+
+    printf("Contenido del puntero: %d\n", *pnumero);
+    printf("Dirección de memoria almacenada por el puntero: %p\n", pnumero);
+    printf("la dirección de memoria de la variable.: %p\n", &numero);
+    printf("la dirección de memoria del puntero.: %p\n", &pnumero);
+
+    printf("Tamaño de la variable numero: %d\n", sizeof(numero));
+
     return 0;
 }
