cb47850555af06000e7c95d7a1596a2fa87e5cfd
Bruno Cordoba <brunonahuel87@gmail.com>
2025-03-26 08:58:21 -0300

    precedimiento creado

--- tp1_2.c
+++ tp1_2.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int NumeroCuadrado(int numero);
+void NumeroCuadrado2(int numero);
 
 int main()
 {
+   /*   funcion 1
+
     int numero;
     printf("Ingrese un numero: ");
     scanf("%d", &numero);
 
-    printf("El cuadrado de %d es: %d", numero, NumeroCuadrado(numero));
+    printf("El cuadrado de %d es: %d", numero, NumeroCuadrado(numero));*/
+
+    /*  funcion 2
+    NumeroCuadrado2(4);*/
+
+    
+
+
 
     return 0;
 }
@@ -16,4 +26,9 @@ int main()
 int NumeroCuadrado(int numero)
 {
     return numero * numero;
+}
+
+void NumeroCuadrado2(int numero)
+{
+    printf("El cuadrado de %d es: %d", numero, numero * numero);  
 }
